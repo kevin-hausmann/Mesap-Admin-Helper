bd3b969464dd1a1b633c94c8ce52d4cda0699c6d
Kevin Hausmann <kevin.hausmann@uba.de>
2017-08-31 16:18:23 +0200

    Implement interpolation and virtual formula checks

--- UBA MESAP Admin Helper Application/Types/QualityChecks/TimeSeries/Properties/InterpolationCheck.cs
+++ UBA MESAP Admin Helper Application/Types/QualityChecks/TimeSeries/Properties/InterpolationCheck.cs
@@ -15,17 +15,44 @@ namespace UBA.Mesap.AdminHelper.Types.QualityChecks
         
         protected override int[,] FindWorkloadFilter => new int[,]
         {
-            {(int)DimensionEnum.Type, (int)DescriptorEnum.EF},
+            {(int)DimensionEnum.Type, (int)DescriptorEnum.AD, (int)DescriptorEnum.EF, (int)DescriptorEnum.EM},
         };
         
         protected override void CheckTimeSeries(TimeSeries series, IProgress<ISet<Finding>> progress)
         {
+            int type = 0;
+            int area = 0;
+            foreach (dboTSKey key in series.Object.TSKeys)
+                if (key.DimNr == (int)DimensionEnum.Type)
+                    type = key.ObjNr;
+                else if (key.DimNr == (int)DimensionEnum.Area && area == 0)
+                    area = key.ObjNr;
+
             series.Object.DbReadRelatedProperties();
             dboTSProperty property = series.Object.TSProperties.GetObject(mspTimeKeyEnum.mspTimeKeyYear, mspTimeKeyTypeEnum.mspTimeKeyTypeUnknown);
-            if ((int)property.Interpol > 1)
-                Report(progress, new TimeSeries[] { series },
-                    String.Format(FindingTitle, series.ID, property.Interpol),
-                    String.Format(FindingText, series.Legend));
+
+            if (type == (int)DescriptorEnum.EF && area != (int)DescriptorEnum.Germany)
+            {
+                if (property.Interpol > mspInterpolEnum.mspInterpolLinear || property.Extrapol != mspExtrapolEnum.mspExtrapolNone)
+                    Report(progress, new TimeSeries[] { series },
+                        String.Format(FindingTitle, "ABL/NBL-EF", series.ID, property.Interpol + ", " + property.Extrapol),
+                        String.Format(FindingText, series.Legend));
+            }
+            else if (type == (int)DescriptorEnum.EF)
+            {
+                if (property.Interpol > mspInterpolEnum.mspInterpolLinear)
+                    Report(progress, new TimeSeries[] { series },
+                        String.Format(FindingTitle, "EF", series.ID, property.Interpol + ", " + property.Extrapol),
+                        String.Format(FindingText, series.Legend));
+            }
+            else 
+            {
+                if (property.Interpol > mspInterpolEnum.mspInterpolNone || property.Extrapol > mspExtrapolEnum.mspExtrapolNone)
+                    Report(progress, new TimeSeries[] { series },
+                        String.Format(FindingTitle, type == (int)DescriptorEnum.AD ? "AR" : "EM", series.ID, property.Interpol + ", " + property.Extrapol),
+                        String.Format(FindingText, series.Legend));
+            }
+
         }
     }
 }
---
+++ UBA MESAP Admin Helper Application/Types/QualityChecks/TimeSeries/Properties/VirtualFormulaCheck.cs
@@ -0,0 +1,63 @@
+﻿using M4DBO;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace UBA.Mesap.AdminHelper.Types.QualityChecks
+{
+    class VirtualFormulaCheck : TimeSeriesQualityCheck
+    {
+        public override string Id => "Virtuelle";
+
+        public override TimeSpan EstimatedExecutionTimePerElement => TimeSpan.FromMilliseconds(25);
+        
+        protected override short StartYear => 1990;
+        protected override short EndYear => 1995;
+        
+        protected override int[,] FindWorkloadFilter => new int[,] {};
+        
+        protected override void CheckTimeSeries(TimeSeries series, IProgress<ISet<Finding>> progress)
+        {
+            if (series.Object.VirtualType == mspVirtualTsTypeEnum.mspVirtualTsTypeVirtual)
+            {
+                // Find all IDs in Formula
+                string[] ids = GetIDsUsedInFormula(series.Object.VirtualFormula); 
+
+                // Make sure all referenced time series exist
+                dboTSs allReferencedSeries = series.Object.Database.CreateObject_TSs(String.Join(",", ids));
+                if (allReferencedSeries.Count != ids.Length)
+                    Report(progress, new TimeSeries[] { series },
+                            String.Format(FindingTitle, series.ID),
+                            String.Format(FindingText, series.Object.VirtualFormula, series.Legend));
+            }
+        }
+
+        private string[] GetIDsUsedInFormula(string formula)
+        {
+            List<string> ids = new List<string>();
+
+            // Remove all unit stuff
+            foreach (Match match in Regex.Matches(formula, @"\[(.*?)\]"))
+                formula = formula.Replace(match.ToString(), String.Empty);
+
+            // Extract IDs in double quotes
+            foreach (Match match in Regex.Matches(formula, "\"([^\"]*)\""))
+            {
+                ids.Add(match.ToString().Replace("\"", String.Empty));
+                formula = formula.Replace(match.ToString(), String.Empty);
+            }
+            
+            // Remove all math stuff
+            var reserved = new string[] { "(", ")", "[", "]", "*", "+", "-", "/", "NaNTo0" };
+            foreach (var c in reserved)
+                formula = formula.Replace(c, " ");
+            
+            // Find all non-quoted IDs and drop numerics
+            foreach (string id in formula.Split((char[])null, StringSplitOptions.RemoveEmptyEntries))
+                if (!Double.TryParse(id, out double result) && !ids.Contains(id))
+                    ids.Add(id);
+
+            return ids.ToArray();
+        }
+    }
+}
